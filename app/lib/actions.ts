c7fb17d74ef1a06f7ca9efc0c94dc20eae7d81f3
Michael Trang <michael.trang@hotmail.com>
2023-12-28 23:56:36 +0000

    complete chapter 14: Improve form validation and accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,24 +7,50 @@ import {z} from 'zod';
 
 const FormSchema = z.object({
 	id: z.string(),
-	customerId: z.string(),
-	amount: z.coerce.number(),
-	status: z.enum(['pending', 'paid']),
+	customerId: z.string({
+		invalid_type_error: 'Please select a customer',
+	}),
+	amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0.'}),
+	status: z.enum(['pending', 'paid'], {
+		invalid_type_error: 'Please select a status',
+	}),
 	date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
+// This is temporary until @types/react-dom is updated
+export type State = {
+	errors?: {
+		customerId?: string[];
+		amount?: string[];
+		status?: string[];
+	};
+	message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
 	const rawFormData = {
-		customerId: formData.get('customerId') || '',
-		amount: formData.get('amount') || 0,
-		status: formData.get('status') || 'pending',
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
 	}
 
-	// console.log('rawFormData', rawFormData);
+	console.log('rawFormData', rawFormData);
 
-	const {customerId, amount, status} = CreateInvoice.parse(rawFormData);
+	const validateFields = CreateInvoice.safeParse(rawFormData);
+
+	console.log('validateFields', validateFields);
+
+	if (!validateFields.success) {
+		return {
+			errors: validateFields.error.flatten().fieldErrors,
+			message: 'Invalid Form Submission. Failed to Create Invoice.',
+		};
+	}
+
+	// Prepare data for insertion into the database
+	const {customerId, amount, status} = validateFields.data;
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split('T')[0];
 
@@ -50,14 +76,24 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
 	const rawFormData = {
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	}
 
-	const {customerId, amount, status} = UpdateInvoice.parse(rawFormData);
+	const validateFields = UpdateInvoice.safeParse(rawFormData);
+
+	if (!validateFields.success) {
+		return {
+			errors: validateFields.error.flatten().fieldErrors,
+			message: 'Invalid Form Submission. Failed to Update Invoice.',
+		};
+	}
+
+	// Prepare data for insertion into the database
+	const {customerId, amount, status} = validateFields.data;
 	const amountInCents = amount * 100;
 
 	try {
@@ -73,6 +109,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 			message: 'Database Error: Failed to Update Invoice',
 		}
 	}
+
+  revalidatePath('/dashboard/invoices');
 	redirect('/dashboard/invoices');
 }
 
